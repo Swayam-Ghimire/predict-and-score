62310d4812e9e9c4b6627abd544d40e290901388
Swayam <subinghi22@gmail.com>
2025-01-27 13:01:06 +0545

    Started creating the home page

--- home.php
+++ home.php
@@ -1,11 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="keywords" content="Liverpool, prediction game, football, liverpool football, registration">
+    <meta name="description" content="registration and login system">
+    <meta name="author" content="Swayam Ghimire">
+    <title>Upcoming Fixtures</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
+    <link rel="stylesheet" href="home.css">
 </head>
+
 <body>
-    Hellow world
+    <header>
+        <nav class="navbar">
+            <div id="logo">
+                <img src="images/liverpool.jpg" alt="logo">
+            </div>
+            <ul>
+                <li class="item"><a href="#main">Fixtures</a></li>
+                <li class="item"><a href="#">Leaderboard</a></li>
+                <li class="item"><a href="logout.php">Logout</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="main">
+        <div class="table_header">
+            <h1>Upcoming Fixtures</h1>
+        </div>
+        <div class="table">
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th class="head">Id</th>
+                        <th class="head">Match</th>
+                        <th class="head">Date and Time</th>
+                        <th class="head">Prediction</th>
+                        <th class="head">Actions</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+
+    </section>
 </body>
+
 </html>
